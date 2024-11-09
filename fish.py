277c9268cb8c55c7b2fc9c01b4386053b41411a7
BombaSter700 <azazmaz.228@gmail.com>
2024-11-09 17:20:14 +0500

    lab_2_dont_work_out_enemy

--- fish.py
+++ fish.py
@@ -1,7 +1,16 @@
 import pygame
 import random
+import sys
 
-pygame.init()
+def overlap(x,y,w,h,vx,vy,vw,vh):
+    x2=x+w
+    y2=y+h
+    vx2=vx+vw 
+    vy2=vy+vh
+    r = (vx<=x<=vx2 or x<=vx<=x2) and (vy<=y<=vy2 or y<=vy<=y2)
+    return r
+
+pygame.init() #инизиализация игры
 
 # Параметры окна
 s_width = 1000  # ширина игрового окна
@@ -17,7 +26,7 @@ player_y = 30  # начальная координата y
 player_size = 30  # начальный размер игрока
 player_left = False  # направление движения игрока
 
-enemy_n = 10  # количество врагов
+enemy_n = 20  # количество врагов
 enemy_pic = pygame.image.load("Fish04_A.png")  # картинка врагов
 enemy_pic2 = pygame.image.load("Fish04_B.png")  # альтернативная картинка врагов
 
@@ -34,6 +43,8 @@ for i in range(enemy_n):
     enemy_x[i] = random.randint(0, s_width)
     enemy_y[i] = random.randint(0, s_high)
     enemy_tick[i] = random.randint(0, 10)
+    enemy_speed[i]=random.randint(-3,3)  # задачем случайную скорость врагов
+    enemy_size[i]=random.randint(15,60)  # задачем случайный размер врагов
 
 # Фон
 background_pic = pygame.image.load("Scene_A.png")
@@ -44,7 +55,19 @@ b_tick = 50  # счетчик тиков для смены фона
 clock = pygame.time.Clock()
 running = True
 
-# Основной цикл игры
+#инициализция переменные пузырей
+bubble_n = 10 # задаем количество пузырей
+bubble_pic = pygame.image.load("Bubble.png")
+bubble_x=[0]*bubble_n
+bubble_y=[0]*bubble_n
+bubble_size=[0]*bubble_n
+#заполняем координаты пузырей и размеры случайными числами
+for i in range(bubble_n):
+    bubble_x[i]= random.randint(0, s_width)
+    bubble_y[i]= random.randint(s_high, s_high*2)
+    bubble_size[i]= random.randint(10,32)
+
+'''******************** ОСНОВНОЙ ЦИКЛ ИГРЫ **********************'''
 while running:
     # Обработка событий
     for event in pygame.event.get():
@@ -86,7 +109,22 @@ while running:
     screen.blit(player_pic_small, (player_x, player_y))
 
     # Вывод врагов
-    for i in range(enemy_n):
+    for i in range(enemy_n): 
+        # проверяем нет ли столкновениия рыбки врага с рыбкой героем
+        if overlap(player_x, player_y int(player_size * 1.25), player_size, enemy_x[i], enemy_y[i], 
+                   int(enemy_size[i]*1.25), enemy_size[i]):
+            if player_size>enemy_size[i]:
+                player_size += 2   # увеличиваем размер героя
+                enemy_x[i] = -200  # прячем врага за левую границу экрана
+                enemy_speed[i]=2   # задаем положительную скорость движения
+            else:
+                enemy_size[i] +=10 # увеличиваем размер врага
+                player_x = 15      # задаем начальную х-координату героя
+                player_y = 30      # задаем начальную y-координату героя
+                player_size=30     # задаем начальный размер героя
+                player_left=False  # указываем, что герой смотрит направо
+
+
         # Обновление счетчика тиков врагов
         enemy_tick[i] -= 1
         if enemy_tick[i] == 0:
